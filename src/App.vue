b6686715192f6dd8242aeaea67984130d8ed0dd0
zhanghengxin <ezreal.ice@icloud.com>
2022-09-16 18:49:09 +0800

    cors env

--- src/App.vue
+++ src/App.vue
@@ -1,188 +1,197 @@
 <!--
- * @Author: JerryK
- * @Date: 2021-09-18 21:32:13
- * @LastEditors: Jerryk jerry@icewhale.org
- * @LastEditTime: 2022-08-05 14:59:42
- * @Description: Main entry of application
- * @FilePath: /CasaOS-UI/src/App.vue
+ * @Author: zhanghengxin hengxin.zhang@icewhale.org
+ * @Date:  2022-09-13 17:01:37
+ * @LastEditors: zhanghengxin hengxin.zhang@icewhale.org
+ * @LastEditTime: 2022-09-13 17:01:37
+ * @FilePath: src/components/Storage/StorageSettings.vue
+ * @Description:
+ *
+ * Copyright (c) 2022 by IceWhale, All Rights Reserved.
 -->
-
 <template>
-  <div id="app" class="is-flex is-flex-direction-column" :class="{'is-dark-bg':$route.meta.showBackground}">
-    <template v-if="$route.meta.showBackground">
-      <!-- Background Layer Start -->
-      <casa-wallpaper :animate="isWelcome?initAni:noneAni"></casa-wallpaper>
-      <!-- Background Layer End -->
+  <div class="modal-card">
+    <!--    head-->
+    <header class="modal-card-head b-line">
+      <div class="is-flex-grow-1">
+        <h3 class=" title is-3">{{ $t('MainStorage Settings') }}</h3>
+      </div>
+      <button class="delete" type="button" @click="$emit('close')"/>
+    </header>
+    <!--remind-->
+    <div class="ml-6">
+      {{ $t('Please choose a location with enough storage space and stable connection.') }}
+    </div>
+    <div class="ml-6">
+      {{ $t('All the checked Storage will be merged into one big Storage.') }}
+    </div>
+    <div v-if="lessThen2" class="pri-message-alert ml-6 mr-6 is-flex is-align-items-center">
+      <div class="left ml-4 mr-2">
+        <b-icon class="is-16x16" icon="danger" pack="casa"></b-icon>
+      </div>
+      {{ $t('Please select at least 2 Storages.') }}
+    </div>
 
-      <!-- BrandBar Start -->
-      <brand-bar v-animate-css="brandAni" v-if="!$store.state.isMobile"></brand-bar>
-      <!-- BrandBar End -->
-      <!-- ContactBar Start -->
-      <contact-bar v-animate-css="contactAni" v-if="!$store.state.isMobile"></contact-bar>
-      <!-- ContactBar End -->
-    </template>
+    <div class="pri-message-danger ml-6 mr-6 is-flex is-align-items-center">
+      <div class="left ml-4 mr-2">
+        <b-icon class="is-16x16" icon="danger" pack="casa"></b-icon>
+      </div>
+      {{ $t('Please back up your data in storage, otherwise the data may be lost.') }}
+    </div>
 
-    <!-- Router View Start -->
-    <router-view />
-    <!-- Router View End -->
+    <!--    body-->
+    <!--    <section class="modal-card-body">-->
+    <!--    <section class="notification is-overlay pri-margin">-->
+    <section class="notification is-overlay mt-2 mr-6 mb-6 ml-6 pr-0">
+      <div v-for="(item, index) in storageData" :key="index" class="is-flex pt-2 pb-2">
+        <div>
+          <b-image :src="require('@/assets/img/storage/storage.png')" class="is-64x64"></b-image>
+        </div>
+        <div class="is-flex is-flex-grow-1 is-flex-direction-column is-justify-content-center ">
+          <span class="is-uppercase one-line is-size-14px">{{ item.name }}</span>
+        </div>
+        <div class="is-flex is-flex-shrink-0 is-flex-direction-column is-justify-content-center">
+          <span class="is-uppercase is-size-7 pri-text-color">{{ renderSize(item.size - item.availSize) }}/{{
+              renderSize(item.size)
+            }}</span>
+        </div>
+        <b-checkbox v-model="checkBoxGroup" :native-value="item.path" class="ml-2 mr-4" type="is-info"></b-checkbox>
+      </div>
+    </section>
 
-    <!-- <v-tour name="myTour" :steps="steps"></v-tour> -->
+    <footer class="modal-card-foot is-flex is-align-items-center">
+      <div class="is-flex-grow-1"></div>
+      <div>
+        <b-button :label="$t('Submit')" :loading="isConnecting" expaned rounded type="is-primary" @click="submit"/>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import BrandBar from './components/BrandBar.vue'
-import ContactBar from './components/ContactBar.vue'
-import CasaWallpaper from './components/wallpaper/CasaWallpaper.vue'
-import { mixin } from './mixins/mixin';
-
-const customIconConfig = {
-  customIconPacks: {
-    'casa': {
-      sizes: {
-        'default': 'is-size-4',
-        'is-20': 'is-size-5',
-        'is-small': '',
-        'is-medium': 'is-size-3',
-        'is-large': 'is-size-1'
-      },
-      iconPrefix: 'casa-',
-      internalIcons: {
-        'check': 'checkmark',
-        'information': 'information',
-        'check-circle': 'checkmark-circle-outline',
-        'alert': 'alert',
-        'alert-circle': 'alert',
-        'arrow-up': 'arrow-up',
-        'chevron-right': 'arrow-right',
-        'chevron-left': 'arrow-back',
-        'chevron-down': 'arrow-down',
-        'eye': 'eye',
-        'eye-off': 'eye-off',
-        'menu-down': 'arrow-dropdown',
-        'menu-up': 'arrow-dropup',
-        'close-circle': 'close-circle-outline'
-      }
-    },
-  }
-}
+import {mixin} from "@/mixins/mixin";
+// import CasaOsLocalStorageApi from "@/codegen/local_storage/dist/index.js";
+// var CasaOsLocalStorageApi = require('casa_os_local_storage_api');
+let CasaOsLocalStorageApi = require('@/codegen/local_storage/dist/index.js')
 
 export default {
-  components: {
-    BrandBar,
-    ContactBar,
-    CasaWallpaper
-  },
+  name: "StorageSettings",
   mixins: [mixin],
+  async mounted() {
+    await this.getDiskList();
+  },
+  computed: {
+    lessThen2() {
+      if (this.checkBoxGroup.length < 2) {
+        return true
+      }
+      return false
+    },
+    extended() {
+      return this.checkBoxGroup.join(":")
+    },
+
+  },
   data() {
     return {
-      //isLoading: true,
-      steps: [
-      ],
-      isWelcome: false,
-      noneAni: {
-        classes: 'fadeIn',
-        duration: 500
-      },
-      initAni: {
-        classes: 'zoomOutIn',
-        duration: 2500
-      },
-      brandAni: {
-        classes: "fadeInLeft",
-        duration: 700
-      },
-      contactAni: {
-        classes: "fadeInRight",
-        duration: 700
-      },
+      storageData: [],
+      storage: [{
+        "Id": 0,
+        "MountPoint": "/DATA",
+        "FSType": "fuse.mergerfs",
+        "Source": "/mnt/a:/mnt/b",
+        "Options": "defaults,allow_other,use_ino,category.create=mfs,moveonenospc=true,minfreespace=1M",
+        "Persist": false,
+        "Extended": {
+          "mergerfs.srcmounts": "/mnt/a:/mnt/b"
+        }
+      }],
+      diskData: {},
+      unDiskData: {},
+      checkBoxGroup: [],
+      isConnecting: false,
+
     }
-  },
-
-
-  computed: {
-    isLoading() {
-      return this.$store.state.siteLoading
-    },
-  },
-
-  created() {
-    console.log(`%c
-_____             _____ _____ 
-|     |___ ___ ___|     |   __|
-|   --| .'|_ -| .'|  |  |__   |
-|_____|__,|___|__,|_____|_____|
--- Made by IceWhale with YOU --
-`, `font-family: monospace`);
-
-    this.$buefy.config.setOptions(customIconConfig)
-    this.checkInit();
-  },
-  mounted() {
-    this.setInitLang();
-    window.addEventListener('resize', this.onWindowResize);
-    this.onWindowResize();
   },
   methods: {
     /**
-     * @description: Check if System need init first
-     * @return {*} void
+     * @description: Get disk list
+     * @param {}
+     * @return {void}
+     * src/components/Storage/StorageManagerPanel.vue:234
      */
-    checkInit() {
-      this.$api.users.getUserStatus().then(res => {
-        if (res.data.success == 200 && !res.data.data.initialized) {
-          this.isWelcome = true
-          this.$store.commit('SET_SITE_LOADING', false)
-          this.$store.commit('SET_NEED_INITIALIZATION', true)
-          this.$store.commit('SET_INIT_KEY', res.data.data.key)
-          localStorage.removeItem("access_token");
-          localStorage.removeItem("refresh_token");
-          this.$router.push("/welcome");
-        } else {
-          this.isWelcome = false
+    async getDiskList() {
+      // get storage list
+      // TODO: the part is repetition
+      //  with APPs Installation Location requirement document
+      const storageRes = await this.$api.storage.list({system: "show"})
+      const storageArray = []
+      storageRes.data.data.forEach(item => {
+        item.children.forEach(part => {
+          part.disk = item.path
+          part.diskName = item.disk_name
+          storageArray.push(part)
+        })
+      })
+      this.storageData = storageArray.map((storage) => {
+        return {
+          name: storage.label,
+          isSystem: storage.diskName == "System",
+          fsType: storage.type,
+          size: storage.size,
+          availSize: storage.avail,
+          usePercent: 100 - Math.floor(storage.avail * 100 / storage.size),
+          diskName: storage.drive_name,
+          path: storage.path,
+          mount_point: storage.mount_point,
+          disk: storage.disk
         }
       })
     },
-    /**
-     * @description: Get and Set default language 
-     * @return {*} void
-     */
-    setInitLang() {
-      let lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : this.getLangFromBrowser()
-      lang = lang.includes("_") ? lang : "en_us";
-      this.setLang(lang);
-    },
-    /**
-     * @description: Handle on Window reize
-     * @return {*}
-     */
-    onWindowResize() {
-      const isMobile = document.body.clientWidth < 480
-      this.$store.commit('SET_IS_MOBILE', isMobile)
-    },
-  },
-  sockets: {
-    connect() {
-      console.log('socket connected');
-    },
 
+    submit() {
+      // MountMethodsApiFactory({mount_point: '/DATA', extended: this.extended}).updateMount('/DATA').then(() => {
+      //   console.log(1231231231231231231231312321312321)
+      // })
+
+      var api = new CasaOsLocalStorageApi.MountMethodsApi()
+      var opts = {
+        // 'id': 0,
+        'mountPoint': '/DATA',
+        // 'type': 'ext4',
+        // 'source': '/dev/sda1'
+      };
+      var callback = function (error, data, response) {
+        debugger
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('API called successfully. Returned data: ' + data);
+        }
+      };
+      api.updateMount(opts, callback);
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-#app {
-  width: 100vw;
-  height: 100vh;
-  font-weight: 400;
-  font-size: 0.875rem;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  overflow-y: hidden;
+.pri-margin {
+  margin: 2rem;
+}
 
-  &.is-dark-bg {
-    background-color: #000;
-  }
+.pri-text-color {
+  color: hsla(0, 0%, 0%, 0.4);
+
+}
+
+.pri-message-danger {
+  height: 2rem;
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  border-radius: 0.25rem;
+  color: hsla(348, 86%, 61%, 1);
+  font-size: 0.875rem;
+  font-style: normal;
+  background: hsla(348, 100%, 95%, 1);
 }
 </style>
